3053463c1b8a1dec6ab7a14b264835d202487471
young <zerobugpark@gmail.com>
2025-01-03 19:18:09 +0900

    Add File
    1. UITableViewController
    2. UITableViewCell

---
+++ ShoppingList/ShoppingTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ShoppingTableViewController.swift
+//  ShoppingList
+//
+//  Created by youngkyun park on 1/3/25.
+//
+
+import UIKit
+
+class ShoppingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+ 
+
+}
