648c7d407cfa4dd2dc2bdd817a44f4a7885ed97d
young <zerobugpark@gmail.com>
2025-01-03 19:06:28 +0900

    Initial Commit

---
+++ ShoppingList/ShoppingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShoppingTableViewCell.swift
+//  ShoppingList
+//
+//  Created by youngkyun park on 1/3/25.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
